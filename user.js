3b54c1b405c6d74d898068fa48d792f1828d4e81
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-21 13:30:47 +0300

    update all

--- user.js
+++ user.js
@@ -1,24 +1,42 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let fs = require('fs');
+var bodyParser = require('body-parser');
+var urlEncoded = bodyParser.urlencoded({extended:false});
+
+
+var data = JSON.parse(fs.readFileSync(__dirname+'/users.json'));
+
+app.get('/form/',function(req,res){
+    res.sendFile(__dirname+'/form.html');
+});
 
 app.get('/',function(req,res){
     res.send("Server Start");
 });
 
 app.get('/list-users',function(req,res){
-    res.send("List Start");
+    res.json(data);
 });
 
 app.get('/user/:id',function(req,res){
-    res.send("Select User");
+    var id = req.params.id;
+    var user = data[id];
+    console.log(user);
+    res.json(user);
 });
 
-app.delete('/delete-user',function(req,res){
+app.get('/delete-user/:id',function(req,res){
+    var id = req.params.id;
     res.send("User Deleted");
 });
 
 app.post('/add-user',function(req,res){
     res.send("User Added");
+});
+
+app.post('/submitform/',urlEncoded,function(req,res){
+    res.send("Data submitted: "+req.body.email+" , "+req.body.password);
 });
 
 var server = app.listen(3355,function(){
